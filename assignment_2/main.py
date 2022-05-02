1ce7f549b37bf028e654182e49602507b9229520
Rodion <chernitca@ya.ru>
2022-05-02 19:43:31 +0300

    ai

---
+++ assignment_2/main.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python3
+import sys, random
+import numpy as np
+from music21 import *
+
+SETTINGS = {
+    'VOLUME': 50,
+    'CHORD_DURATION': 1.0
+}
+
+'''
+Firstly, we need read the midi file and convert it to music21.Stream object.
+
+It will allow to duplicate object keeping the all headers settings from the input midi file.
+Also, it will make possible to change chords sequence not affecting the other populations.
+'''
+def read_midi(path: str) -> stream.Stream:
+    print(f'Reading midi file {path}...')
+    midi_file = converter.parse(path)
+    print('Done!')
+    return midi_file
+
+'''
+Returns tone of the melody in midi file.
+'''
+def get_midi_tone(midi_file: stream.Stream) -> tuple[str]:
+    key = midi_file.analyze('key')
+    return (key.tonic.name, key.mode)
+
+'''
+Set volume for the accompaniment.
+Count average between the notes and takes away some points.
+'''
+def set_volume(midi: stream.Stream, adjust: int):
+    SETTINGS['VOLUME'] = np.average(np.array([_.volume.velocity for _ in midi.recurse() if type(_) is note.Note])) - adjust
+
+'''
+Simply make random chord in the range of two octaves.
+Duration is in ratios (в долях), e.g. quarter, whole etc.
+'''
+def make_random_chord(volume_: int, octave_: int, duration_: float) -> chord.Chord:
+    c = chord.Chord()
+    
+    notes = random.sample(range(0,23), 3)
+    n1 = note.Note(notes.pop() + (octave_ * 12))
+    n2 = note.Note(notes.pop() + (octave_ * 12))
+    n3 = note.Note(notes.pop() + (octave_ * 12))
+    n1.volume.velocity = volume_
+    n2.volume.velocity = volume_
+    n3.volume.velocity = volume_
+
+    c.duration = duration.Duration(duration_)
+    c.add([n1,n2,n3])
+
+    return c
+
+'''
+Generate the chord sequence using the random chords generator.
+'''
+def make_chord_seq(count_: int) -> list[chord.Chord]:
+    ret: list[chord.Chord] = []
+    for _ in range(0, count_):
+        ret.append(make_random_chord(SETTINGS['VOLUME'], 4, 1.0))
+    return ret
+
+'''
+Make population melody + chords.
+'''
+def make_chord_track(chords: list[chord.Chord]) -> stream.Stream:
+    cstream = stream.Stream()
+    for _ in chords:
+        cstream.append(_)
+    return cstream
+
+'''
+If needs to determine length of the midi,
+i.e. to understand how many bars (тактов) in the song.
+This information allows to count the chords number.
+'''
+def get_bars_count(song: stream.Stream) -> int:
+    return int(np.sum(np.array([1 for _ in song.recurse() if type(_) is stream.Measure])))
+
+def save_midi(song: stream.Stream, fname='output.mid'):
+    song.write('midi', fp=fname)
+
+def main(infile: str, outfile: str):
+    mstream = read_midi(infile)
+    set_volume(mstream, adjust = 15)
+    bars = get_bars_count(mstream)
+    track = make_chord_track(make_chord_seq(bars * int(bars / SETTINGS['CHORD_DURATION'])))
+    track.instrument = instrument.Piano()
+    mstream.append(track)
+    save_midi(mstream, outfile)
+
+if __name__ == '__main__':
+    # Check if we are inside the virtualenv
+    # import os
+    # print(os.environ['VIRTUAL_ENV'])
+    main(sys.argv[1], sys.argv[2])
